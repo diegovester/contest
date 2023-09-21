990a92bfc67fc57290b81f733913a7fd008aaace
Thinh Nguyen <66584706+tnn3437@users.noreply.github.com>
2023-09-21 16:37:49 -0500

    Update and rename Palindromic_String.py to 3.Palindromic_String.py

--- bonus/3.Palindromic_String.py
+++ bonus/3.Palindromic_String.py
@@ -1,10 +1,12 @@
 # Write a function to check if a string is a palindrome using recursion
 
-#RECURSION is the only accepted method
+# A string is called a palindrome if the reverse of the string is the same as the original one.
+
+# RECURSION is the only accepted method
 
 # 5 PTS
 
-def is_palindrome(s: str) -> bool:
+def is_palindrome(s):
   #YOUR CODE HERE 
 
 
