cd9c7f0981576befe40e2c903d45f2a0879d8b2c
Thinh Nguyen <66584706+tnn3437@users.noreply.github.com>
2023-09-21 16:44:10 -0500

    Create 4.Count_Zeros.py

---
+++ bonus/4.Count_Zeros.py
@@ -0,0 +1,9 @@
+# Given an integer n, return the number of zero(s) in n using recursion.
+# 5 pts
+
+def count_zeros(n):
+  # YOUR CODE HERE
+
+
+print("number of zero(s) in 09212023 is ", count_zeros(2020))
+print("number of zero(s) in 1001001 is ", count_zeros(1110))
