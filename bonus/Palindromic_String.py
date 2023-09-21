19083de0d8ca82818ccb5cd68ffe0c11fd8bdc66
Thinh Nguyen <66584706+tnn3437@users.noreply.github.com>
2023-09-21 16:32:37 -0500

    Update and rename bonus1 to Palindromic_String.py

---
+++ bonus/Palindromic_String.py
@@ -0,0 +1,13 @@
+# Write a function to check if a string is a palindrome using recursion
+
+#RECURSION is the only accepted method
+
+# 5 PTS
+
+def is_palindrome(s: str) -> bool:
+  #YOUR CODE HERE 
+
+
+print(is_palindrome("racecar"))      #True
+print(is_palindrome("1233210"))      #False
+  
